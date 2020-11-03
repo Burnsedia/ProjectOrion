555c38df43d8ae1ccf26f1dd61411c5b45d4cb7f
burnsedia <bburnsed1995@gmail.com>
2020-11-03 11:53:45 -0500

    init commit

---
+++ scripts/ship.gd
@@ -0,0 +1,17 @@
+extends KinematicBody
+class_name ship
+
+
+export onready var speed:int = 64
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
